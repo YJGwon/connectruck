8a136562823da31ba94916e61e325716d97e3c39
Forky <trulyforky@gmail.com>
2023-06-13 23:54:50 +0900

    feat(ui-trucks): 푸드트럭 목록 조회 ui (#8)
    close #1
    
    * feat: 푸드트럭 목록 조회 ui
    
    * feat: 트럭 목록 조회 api 요청 전송
    
    * feat: 트럭 목록 무한 스크롤 적용
    
    * feat: thumbnail 없을 경우 sample image로 대체

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TruckList from './component/TruckList';
+
+function App() {
+  return (
+    <div>
+      <h1>Connectruck 🚚</h1>
+      <TruckList />
+    </div>  
+  );
+}
+
+export default App;
---
+++ frontend/src/component/TruckList.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+
+class TruckList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      trucks: [],
+      page: 0,
+      size: 20,
+      hasNext: true,
+      isLoading: false // 추가 트럭 데이터 로딩중인지
+    };
+
+    this.fetchTruckData();
+    this.scrollRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // Attach event listener to the scroll container
+    this.scrollRef.current.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    // Remove event listener when component is unmounted
+    this.scrollRef.current.removeEventListener('scroll', this.handleScroll);
+  }
+
+  fetchTruckData = () => {
+    const { trucks, page, size } = this.state;
+
+    this.setState({ isLoading: true });
+
+    // Construct the API endpoint URL with request parameters
+    const url = `http://localhost:8080/api/trucks?page=${page}&size=${size}`;
+
+    // Fetch restaurant data from API
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const { page, trucks } = data;
+        this.setState(prevState => ({
+          trucks: prevState.trucks.concat(trucks),
+          page: page.currentPage + 1,
+          isLoading: false,
+          hasNext: page.hasNext
+        }));
+      })
+      .catch(error => {
+        console.error('Error fetching truck data:', error);
+      });
+  }
+
+  handleScroll = () => {
+    const { isLoading, hasNext } = this.state;
+    const scrollContainer = this.scrollRef.current;
+
+    // Check if scroll container has reached the bottom
+    if (scrollContainer.scrollTop + scrollContainer.clientHeight >= scrollContainer.scrollHeight - 20) {
+      // Load more trucks if not currently loading and there are more to load
+      if (!isLoading && hasNext) {
+        this.fetchTruckData();
+      }
+    }
+  };
+
+  render() {
+    const { trucks, isLoading, hasNext } = this.state;
+
+    return (
+      <div ref={this.scrollRef} style={{ height: '400px', overflow: 'auto' }}>
+        <h1>푸드트럭</h1>
+
+        {trucks.map((truck, index) => (
+          <div className="truck-listing" key={index}>
+            <img 
+              className="truck-thumbnail" 
+              src={truck.thumbnail || 'https://cdn.pixabay.com/photo/2020/06/02/12/12/sample-5250731_1280.png'} 
+              alt={truck.name} 
+            />
+            <div className="truck-info">
+                <h2 className="truck-name">{truck.name}</h2>
+                <p className="truck-location">Location: {truck.location}</p>
+                <p className="truck-hours">Operating Hours: {truck.openHour} - {truck.closeHour}</p>
+            </div>
+          </div>
+        ))}
+        {isLoading && <p>불러오는 중...</p>}
+        {!isLoading && !hasNext && <p>끝</p>}
+      </div>
+    );
+  }
+}
+
+export default TruckList;
