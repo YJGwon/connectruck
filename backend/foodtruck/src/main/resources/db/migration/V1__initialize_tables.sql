0fbc8aedde455852f6a91d596027f95990e90a6c
YJGwon <trulyforky@gmail.com>
2023-06-13 19:57:33 +0900

    chore(database): truck의 thumbnail column type TEXT로 변경

--- backend/foodtruck/src/main/resources/db/migration/V1__initialize_tables.sql
+++ backend/foodtruck/src/main/resources/db/migration/V1__initialize_tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE truck (
     truck_id BIGINT(20) NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    close_hour TIME NOT NULL,
     open_hour TIME NOT NULL,
+    close_hour TIME NOT NULL,
     location VARCHAR(255) NOT NULL,
-    thumbnail VARCHAR(255),
+    thumbnail TEXT,
     PRIMARY KEY (truck_id)
 );
