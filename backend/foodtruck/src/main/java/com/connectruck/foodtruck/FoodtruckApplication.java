0d97ec019e9c98c02b1ee6630d7ea5c9ccf641ca
YJGwon <trulyforky@gmail.com>
2023-06-16 00:30:51 +0900

    test: workflow test

--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/FoodtruckApplication.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/FoodtruckApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FoodtruckApplication {
 
     public static void main(String[] args) {
+        // testing workflow
         SpringApplication.run(FoodtruckApplication.class, args);
     }
 }
