c08191e6df05d5acb6d0c84ed34cfaca57a6f3e7
YJGwon <trulyforky@gmail.com>
2023-06-23 22:00:05 +0900

    feat(ui): 로그인 요청 전송

--- frontend/src/component/loginform/LoginForm.jsx
+++ frontend/src/component/loginform/LoginForm.jsx
@@ -7,11 +7,40 @@ export default function LoginForm({url}) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleFormSubmit = (e) => {
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
-        console.log('url:', `${process.env.REACT_APP_API_URL}${url}`);
-        console.log('id:', username);
-        console.log('password:', password);
+
+        if (username.trim() === '' || password.trim() === '') {
+            alert('아이디와 비밀번호를 입력하세요.');
+            return;
+        }
+
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}${url}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username, password})
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const accessToken = data.accessToken;
+                // Save the access token to local storage
+                localStorage.setItem('accessToken', accessToken);
+            } else {
+                throw new Error(`api error: ${response.json().title}`);
+            }
+        } catch (error) {
+            console.error('Error fetching truck data:', error);
+            if (error.message.startsWith('api error:')) {
+                alert(error.message);
+            } else {
+                alert('로그인 하지 못하였습니다.');
+            }
+        }
+
         setUsername('');
         setPassword('');
     };
@@ -23,14 +52,14 @@ export default function LoginForm({url}) {
                 label="아이디"
                 variant="outlined"
                 value={username}
-                onChange={(e) => setUsername(e.target.value)}/>
+                onChange={(e) => setUsername(e.target.value.trim())}/>
             <TextField
                 className="textField"
                 label="비밀번호"
                 variant="outlined"
                 type="password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}/>
+                onChange={(e) => setPassword(e.target.value.trim())}/>
             <Button className="button" variant="contained" color="primary" type="submit">
                 로그인
             </Button>
