f87f8e366bea9afd54055229180f57ce73470c99
YJGwon <trulyforky@gmail.com>
2023-06-23 22:49:04 +0900

    feat(ui-owners): 로그인 안했을 경우 로그인 페이지로 redirect

--- frontend/src/page/owners/main/OwnersMain.jsx
+++ frontend/src/page/owners/main/OwnersMain.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './OwnersMain.css';
 
 import TopBar from '../../../component/topbar/TopBar';
 
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import LoginForm from '../../../component/loginform/LoginForm';
 
 export default function OwnersMain() {
+    // topbar props
     const title = '사장님 서비스 🚚';
     const home = "/owners";
     const buttons = [
@@ -16,13 +17,23 @@ export default function OwnersMain() {
         }
     ];
 
+    // 로그인 안했을 때 로그인 페이지로 redirect
+    const navigate = useNavigate();
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken');
+        if (!accessToken) {
+            navigate('/owners/signin');
+        }
+    }, [navigate]);
+  
     return (
         <>
-            <TopBar title={title} home={home} buttons={buttons}/>
+            <TopBar title={title} home={home} buttons = {buttons} />
             <div className='owners-main'>
                 <div className="content">
                     <Routes>
-                        <Route exact='exact' path='/' element={<LoginForm url='/api/owners/signin' />}/>
+                        <Route exact='exact' path='/' element="사장님 페이지"/>
+                        <Route path='/signin' element={<LoginForm url = '/api/owners/signin' />}/>
                     </Routes>
                 </div>
             </div>
