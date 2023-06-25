95a2474cb568d929f89fae1b9058fe17c4fe5db8
YJGwon <trulyforky@gmail.com>
2023-06-26 01:00:31 +0900

    chore(scripts): 배포 script 수정
    jar 파일 복사 시 덮어쓰기

--- backend/foodtruck/scripts/deploy-dev.sh
+++ backend/foodtruck/scripts/deploy-dev.sh
@@ -8,12 +8,16 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 # 기존 app 종료
-echo "$TIME_NOW > 실행중인 app 종료" >> $DEPLOY_LOG
+echo "$TIME_NOW > 실행중인 app 종료" > $DEPLOY_LOG
 fuser -k -n tcp 8080
+
+# 기존 jar 파일 삭제
+echo "$TIME_NOW > $JAR_FILE 파일 삭제" >> $DEPLOY_LOG
+rm $JAR_FILE
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/*.jar $JAR_FILE
+cp  $PROJECT_ROOT/*.jar $JAR_FILE
 
 # jar 실행 권한 설정
 echo "> $JAR_FILE 에 실행권한 추가"
