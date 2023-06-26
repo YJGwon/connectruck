ac5c1e802bd13e2a303287621005bf46a2586368
YJGwon <trulyforky@gmail.com>
2023-06-26 22:51:41 +0900

    feat(auth): api 요청에 대해 인증 interceptor 적용

--- backend/foodtruck/src/test/java/com/connectruck/foodtruck/common/testbase/ControllerTestBase.java
+++ backend/foodtruck/src/test/java/com/connectruck/foodtruck/common/testbase/ControllerTestBase.java
@@ -5,17 +5,20 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import com.connectruck.foodtruck.auth.service.AuthService;
+import com.connectruck.foodtruck.auth.support.JwtTokenProvider;
 import com.connectruck.foodtruck.truck.service.TruckService;
 import com.connectruck.foodtruck.user.sevice.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 @WebMvcTest
+@Import(JwtTokenProvider.class)
 public abstract class ControllerTestBase {
 
     @Autowired
