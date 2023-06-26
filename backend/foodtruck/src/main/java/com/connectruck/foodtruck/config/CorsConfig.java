331abe509ebd0efa5b2bb1e4d953c1353cef5937
YJGwon <trulyforky@gmail.com>
2023-06-26 21:47:48 +0900

    refactor: WebConfig -> CorsConfig로 rename

--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/config/CorsConfig.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/config/CorsConfig.java
@@ -2,17 +2,17 @@ package com.connectruck.foodtruck.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
     private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     private final String allowedOrigin;
 
-    public WebConfig(@Value("${connectruck.cors.allowed-origin}") final String allowedOrigin) {
+    public CorsConfig(@Value("${connectruck.cors.allowed-origin}") final String allowedOrigin) {
         this.allowedOrigin = allowedOrigin;
     }
 
