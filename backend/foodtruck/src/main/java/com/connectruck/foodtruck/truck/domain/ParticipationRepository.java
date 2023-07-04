220fd66b5a9a5e383fbc0a09209682e44a6a884c
YJGwon <trulyforky@gmail.com>
2023-07-04 17:58:32 +0900

    feat(truck): 트럭 단일 조회 repository 구현

--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/truck/domain/ParticipationRepository.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/truck/domain/ParticipationRepository.java
@@ -1,10 +1,13 @@
 package com.connectruck.foodtruck.truck.domain;
 
+import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.repository.Repository;
 
 public interface ParticipationRepository extends Repository<Participation, Long> {
+
+    Optional<Participation> findById(final Long id);
 
     Slice<Participation> findByEventId(final Long eventId, final Pageable pageable);
 }
