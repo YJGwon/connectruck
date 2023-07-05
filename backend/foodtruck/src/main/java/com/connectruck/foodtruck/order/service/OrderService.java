144cf488f67c5932a3434fb5c0f27b058211df0d
YJGwon <trulyforky@gmail.com>
2023-07-06 01:02:14 +0900

    feat(order): 행사가 운영되고 있지 않을 때 주문하면 예외 발생

--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/service/OrderService.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.connectruck.foodtruck.order.service;
 
+import com.connectruck.foodtruck.event.service.EventService;
 import com.connectruck.foodtruck.menu.dto.MenuResponse;
 import com.connectruck.foodtruck.menu.service.MenuService;
 import com.connectruck.foodtruck.order.domain.OrderInfo;
@@ -7,6 +8,9 @@ import com.connectruck.foodtruck.order.domain.OrderInfoRepository;
 import com.connectruck.foodtruck.order.domain.OrderLine;
 import com.connectruck.foodtruck.order.dto.OrderMenuRequest;
 import com.connectruck.foodtruck.order.dto.OrderRequest;
+import com.connectruck.foodtruck.order.exception.EventClosedException;
+import com.connectruck.foodtruck.truck.service.TruckService;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,10 +24,14 @@ public class OrderService {
     private final OrderInfoRepository orderInfoRepository;
 
     private final MenuService menuService;
+    private final TruckService truckService;
+    private final EventService eventService;
 
     public Long create(final OrderRequest request) {
-        final OrderInfo orderInfo = OrderInfo.ofNew(request.phone());
+        final Long participationId = request.truckId();
+        checkEventOpened(participationId);
 
+        final OrderInfo orderInfo = OrderInfo.ofNew(request.phone());
         final List<OrderLine> orderLines = request.menus()
                 .stream()
                 .map(orderMenuRequest -> createOrderLineOf(orderInfo, orderMenuRequest))
@@ -32,6 +40,13 @@ public class OrderService {
 
         orderInfoRepository.save(orderInfo);
         return orderInfo.getId();
+    }
+
+    private void checkEventOpened(final Long participationId) {
+        final Long eventId = truckService.findEventIdByParticipationId(participationId);
+        if (eventService.isEventClosedAt(eventId, LocalDateTime.now())) {
+            throw new EventClosedException();
+        }
     }
 
     private OrderLine createOrderLineOf(final OrderInfo orderInfo, final OrderMenuRequest orderMenuRequest) {
--- backend/foodtruck/src/test/java/com/connectruck/foodtruck/order/controller/OrderAcceptanceTest.java
+++ backend/foodtruck/src/test/java/com/connectruck/foodtruck/order/controller/OrderAcceptanceTest.java
@@ -2,24 +2,34 @@ package com.connectruck.foodtruck.order.controller;
 
 import static com.connectruck.foodtruck.common.fixture.data.EventFixture.밤도깨비_야시장;
 import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
 import static org.springframework.http.HttpHeaders.LOCATION;
 import static org.springframework.http.HttpStatus.CREATED;
 
 import com.connectruck.foodtruck.common.testbase.AcceptanceTestBase;
 import com.connectruck.foodtruck.event.domain.Event;
+import com.connectruck.foodtruck.event.service.EventService;
 import com.connectruck.foodtruck.menu.domain.Menu;
 import com.connectruck.foodtruck.order.dto.OrderMenuRequest;
 import com.connectruck.foodtruck.order.dto.OrderRequest;
 import com.connectruck.foodtruck.truck.domain.Participation;
 import io.restassured.response.ValidatableResponse;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 public class OrderAcceptanceTest extends AcceptanceTestBase {
 
     private static final String BASE_URI = "/api/orders";
+
+
+    @SpyBean
+    private EventService eventService;
 
     @DisplayName("메뉴 주문")
     @Nested
@@ -31,6 +41,9 @@ public class OrderAcceptanceTest extends AcceptanceTestBase {
             // given
             final Event event = 밤도깨비_야시장.create();
             dataSetup.saveEvent(event);
+            doReturn(false)
+                    .when(eventService)
+                    .isEventClosedAt(eq(event.getId()), any(LocalDateTime.class));
 
             final Participation savedParticipation = dataSetup.saveParticipation(event);
             final Menu savedMenu = dataSetup.saveMenu(savedParticipation);
