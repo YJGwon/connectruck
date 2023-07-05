3fe729c9330d819afb73210c40d2c84ed9ad17fc
YJGwon <trulyforky@gmail.com>
2023-07-05 17:25:08 +0900

    feat(order): 주문 생성 요청 입력값 검증

--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/dto/OrderMenuRequest.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/dto/OrderMenuRequest.java
@@ -1,7 +1,13 @@
 package com.connectruck.foodtruck.order.dto;
 
+import static com.connectruck.foodtruck.common.constant.ValidationMessage.MISSING_REQUIRED_INPUT;
+import static com.connectruck.foodtruck.common.constant.ValidationMessage.SMALLER_THAN_MIN_VALUE;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record OrderMenuRequest(
-        Long menuId,
-        int quantity
+        @NotNull(message = MISSING_REQUIRED_INPUT + " : 메뉴 id") Long menuId,
+        @Positive(message = SMALLER_THAN_MIN_VALUE + " : 수량, 최소값 1") int quantity
 ) {
 }
--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/dto/OrderRequest.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/dto/OrderRequest.java
@@ -1,8 +1,13 @@
 package com.connectruck.foodtruck.order.dto;
 
+import static com.connectruck.foodtruck.common.constant.ValidationMessage.MISSING_REQUIRED_INPUT;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record OrderRequest(
-        Long truckId,
-        List<OrderMenuRequest> menus) {
+        @NotNull(message = MISSING_REQUIRED_INPUT + " : 푸드트럭 id") Long truckId,
+        @NotEmpty(message = MISSING_REQUIRED_INPUT + " : 주문 메뉴") List<@Valid OrderMenuRequest> menus) {
 }
