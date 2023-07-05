23bfe30b8af725dacf9d30b972af92bbb41cbe46
YJGwon <trulyforky@gmail.com>
2023-07-05 19:53:06 +0900

    feat(db, order): 주문(order_info), 주문 메뉴(order_line) table 추가

---
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/domain/OrderInfo.java
@@ -0,0 +1,48 @@
+package com.connectruck.foodtruck.order.domain;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class OrderInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_info_id")
+    @Include
+    private Long id;
+
+    private String phone;
+
+    @OneToMany(mappedBy = "orderInfo", cascade = CascadeType.PERSIST)
+    private List<OrderLine> orderLines = new ArrayList<>();
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
