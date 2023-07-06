d78188f39b8419402bf15e02cc7f3f77b7e8f0b6
YJGwon <trulyforky@gmail.com>
2023-07-06 18:50:48 +0900

    chore(order): swagger description 추가

--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/controller/OrderController.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.connectruck.foodtruck.order.controller;
 import com.connectruck.foodtruck.order.dto.OrderRequest;
 import com.connectruck.foodtruck.order.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,8 @@ public class OrderController {
     private final OrderService orderService;
 
     @Operation(summary = "행사 참가 푸드트럭 메뉴 주문")
+    @ApiResponse(responseCode = "400", description = "운영 중이지 않은 행사 또는 해당 푸드트럭에 없는 메뉴 주문")
+    @ApiResponse(responseCode = "404", description = "존재하지 않는 참가 푸드트럭, 메뉴 id")
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid final OrderRequest request) {
         final Long id = orderService.create(request);
