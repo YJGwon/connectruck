286ed34c9a7b6faa9f7a3dd5ed63e48c887057c4
YJGwon <trulyforky@gmail.com>
2023-07-11 17:00:38 +0900

    feat(auth): token에서 userId 추출하는 argument resolver 추가

--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/auth/config/AuthConfig.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/auth/config/AuthConfig.java
@@ -1,9 +1,12 @@
 package com.connectruck.foodtruck.auth.config;
 
 import com.connectruck.foodtruck.auth.controller.AuthenticationInterceptor;
+import com.connectruck.foodtruck.auth.controller.AuthenticationPrincipalArgumentResolver;
 import com.connectruck.foodtruck.auth.controller.AuthorizationInterceptor;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +18,7 @@ public class AuthConfig implements WebMvcConfigurer {
 
     private final AuthenticationInterceptor authenticationInterceptor;
     private final AuthorizationInterceptor authorizationInterceptor;
+    private final AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -22,5 +26,10 @@ public class AuthConfig implements WebMvcConfigurer {
                 .addPathPatterns(PATH_PATTERN_API);
         registry.addInterceptor(authorizationInterceptor)
                 .addPathPatterns(PATH_PATTERN_API);
+    }
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authenticationPrincipalArgumentResolver);
     }
 }
