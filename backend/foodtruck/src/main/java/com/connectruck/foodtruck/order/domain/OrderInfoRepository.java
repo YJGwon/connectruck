18b7fefe31cbc222a2b1d45194ab078f15ee65dc
YJGwon <trulyforky@gmail.com>
2023-07-12 19:41:51 +0900

    feat(order): 주문 정보 DB 조회

--- backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/domain/OrderInfoRepository.java
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/order/domain/OrderInfoRepository.java
@@ -1,8 +1,11 @@
 package com.connectruck.foodtruck.order.domain;
 
+import java.util.Optional;
 import org.springframework.data.repository.Repository;
 
 public interface OrderInfoRepository extends Repository<OrderInfo, Long> {
 
     OrderInfo save(final OrderInfo orderInfo);
+
+    Optional<OrderInfo> findById(final Long id);
 }
