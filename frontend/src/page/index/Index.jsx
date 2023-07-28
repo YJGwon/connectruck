70a94f34dd7dcc2699528a48e03c8870b0abdb57
Forky <trulyforky@gmail.com>
2023-07-29 00:54:15 +0900

    feat(ui-index): index 페이지에 주문 조회 버튼 추가

--- frontend/src/page/index/Index.jsx
+++ frontend/src/page/index/Index.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import {Button} from '@mui/material';
+import React, {useState} from 'react';
+import {Container, Button, Stack} from '@mui/material';
 
 import TopBar from '../../component/topbar/TopBar';
 
+import {ServiceOrderTrackingModal} from '../service/ordertracking/ServiceOrderTrackingModal';
+
 export default function Index() {
+    const [openModal, setOpenModal] = useState(false);
+
     const title = 'Connectruck 🚚';
     const root = "/";
 
     return (
-        <div>
+        <React.Fragment>
             <TopBar title={title} root={root}/>
-            <div className="container">
+            <Container>
                 <h4>행사 푸드트럭 주문 시스템을 이용하려면 행사에 제공된 url로 접속해주세요.</h4>
-                <Button variant="contained" href="/owner">사장님 페이지</Button>
-            </div>
-        </div>
+                <Stack spacing={2} direction="row">
+                    <Button variant="contained" href="/owner" sx={{marginRight: '1px'}}>사장님 페이지</Button>
+                    <Button variant="contained" onClick={() => setOpenModal(true)}>주문 조회</Button>
+                </Stack>
+            </Container>
+            <ServiceOrderTrackingModal open={openModal} onClose={() => setOpenModal(false)} />
+        </React.Fragment>
     );
 }
