58a01b3cbb138d3b5c0661f5f85a55bd7d3a5f98
YJGwon <trulyforky@gmail.com>
2023-08-03 20:49:00 +0900

    feat(ui-owner): 사장님 메뉴 설명 편집 modal

--- frontend/src/page/owner/menus/OwnerMenuList.jsx
+++ frontend/src/page/owner/menus/OwnerMenuList.jsx
@@ -8,15 +8,22 @@ import {
     Stack,
     Switch,
     FormControlLabel,
-    IconButton
+    IconButton,
+    Modal,
+    TextField,
+    Button
 } from '@mui/material';
 import {Edit} from '@mui/icons-material';
 
 import {UserContext} from '../../../context/UserContext';
-import {fetchData} from '../../../function/CustomFetch';
+import {fetchData, fetchApi} from '../../../function/CustomFetch';
 
 export const OwnerMenuList = () => {
     const [menus, setMenus] = useState([]);
+
+    const [editingMenuId, setEditingMenuId] = useState(null);
+    const [isDescriptionModalOpen, setIsDescriptionModalOpen] = useState(false);
+    const [descriptionInput, setDescriptionInput] = useState('');
 
     const {isInitialized, accessToken} = useContext(UserContext);
 
@@ -41,6 +48,44 @@ export const OwnerMenuList = () => {
         fetchData({url, requestInfo}, onSuccess);
     }
 
+    const handleDescriptionModalOpen = (menuId) => {
+        setEditingMenuId(menuId);
+        setIsDescriptionModalOpen(true);
+    }
+
+    const handleDescriptionModalClose = () => {
+        setEditingMenuId(null);
+        setDescriptionInput('');
+        setIsDescriptionModalOpen(false);
+    }
+
+    const handleSubmitDescription = () => {
+        if (descriptionInput.length > 25) {
+            alert('25자 이내로 입력하세요.');
+            return;
+        }
+
+        const url = `${process.env.REACT_APP_API_URL}/api/owner/menus/${editingMenuId}/description`;
+        const description = descriptionInput;
+        const data = {
+            description
+        };
+        const requestInfo = {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+        const onSuccess = () => {
+            alert('저장되었습니다.');
+            handleDescriptionModalClose();
+            fetchMenus();
+        };
+        fetchApi({url, requestInfo}, onSuccess);
+    }
+
     return (
         <Box sx={{ width: '100%', margin: '1%' }}>
             <h1>메뉴 관리</h1>
@@ -53,7 +98,7 @@ export const OwnerMenuList = () => {
                                 secondary={
                                     <React.Fragment>
                                         {menu.description}
-                                        <IconButton size="small">
+                                        <IconButton size="small" onClick={() => handleDescriptionModalOpen(menu.id)}>
                                             <Edit fontSize="inherit" />
                                         </IconButton>
                                     </React.Fragment>
@@ -70,6 +115,28 @@ export const OwnerMenuList = () => {
                     ))
                 }
             </List>
+            <Modal open={isDescriptionModalOpen} onClose={handleDescriptionModalClose}>
+                <Box className="modalContainer">
+                    <Typography variant="h5" component="h2" className="modalTitle">
+                        메뉴 설명 편집
+                    </Typography>
+                    <TextField
+                        label="메뉴 설명(25자 이내)"
+                        variant="outlined"
+                        fullWidth={true}
+                        value={descriptionInput}
+                        onChange={(e) => setDescriptionInput(e.target.value)}
+                        className="modalTextInput"/>
+
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleSubmitDescription}
+                        className="modalConfirmButton">
+                        저장
+                    </Button>
+                </Box>
+            </Modal>
         </Box>
     );
 }
