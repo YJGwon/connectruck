3b0dd312b5d99f604bcc787e9172881ba6c6961c
YJGwon <trulyforky@gmail.com>
2023-08-03 21:12:36 +0900

    feat(ui-owner): 사장님 메뉴 품절 처리 modal

--- frontend/src/page/owner/menus/OwnerMenuList.jsx
+++ frontend/src/page/owner/menus/OwnerMenuList.jsx
@@ -22,8 +22,12 @@ export const OwnerMenuList = () => {
     const [menus, setMenus] = useState([]);
 
     const [editingMenuId, setEditingMenuId] = useState(null);
+
     const [isDescriptionModalOpen, setIsDescriptionModalOpen] = useState(false);
     const [descriptionInput, setDescriptionInput] = useState('');
+
+    const [isSoldOutModalOpen, setIsSoldoutModalOpen] = useState(false);
+    const [soldOutInput, setSoldOutInput] = useState(null);
 
     const {isInitialized, accessToken} = useContext(UserContext);
 
@@ -86,6 +90,40 @@ export const OwnerMenuList = () => {
         fetchApi({url, requestInfo}, onSuccess);
     }
 
+    const handleSoldOutModalOpen = (soldOut, menuId) => {
+        setSoldOutInput(soldOut);
+        setEditingMenuId(menuId);
+        setIsSoldoutModalOpen(true);
+    }
+
+    const handleSoldOutModalClose = () => {
+        setEditingMenuId(null);
+        setSoldOutInput(null);
+        setIsSoldoutModalOpen(false);
+    }
+
+    const handleSubmitSoldOut = () => {
+        const url = `${process.env.REACT_APP_API_URL}/api/owner/menus/${editingMenuId}/sold-out`;
+        const soldOut = soldOutInput;
+        const data = {
+            soldOut
+        };
+        const requestInfo = {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+        const onSuccess = () => {
+            alert('저장되었습니다.');
+            handleSoldOutModalClose();
+            fetchMenus();
+        };
+        fetchApi({url, requestInfo}, onSuccess);
+    }
+
     return (
         <Box sx={{ width: '100%', margin: '1%' }}>
             <h1>메뉴 관리</h1>
@@ -103,10 +141,12 @@ export const OwnerMenuList = () => {
                                         </IconButton>
                                     </React.Fragment>
                                 }/>
-                            <Stack direction="row" spacing={2}>
+                            <Stack direction="row" spacing={2} justifyContent='center' alignItems='center'>
                                 <Typography>{`${menu.price}원`}</Typography>
                                 <FormControlLabel
-                                    control={<Switch checked={menu.soldOut} color="error" />}
+                                    control={
+                                        <Switch checked={menu.soldOut} color="error" onChange={(e) => handleSoldOutModalOpen(e.target.checked, menu.id)} />
+                                    }
                                     label="품절"
                                     labelPlacement="top"
                                 />
@@ -135,6 +175,29 @@ export const OwnerMenuList = () => {
                         className="modalConfirmButton">
                         저장
                     </Button>
+                </Box>
+            </Modal>
+            <Modal open={isSoldOutModalOpen} onClose={handleSoldOutModalClose}>
+                <Box className="modalContainer" textAlign='center'>
+                    <Typography variant="h5" component="h2" className="modalTitle">
+                        {soldOutInput ? '품절 처리 하시겠습니까?' : '품절 해제 하시겠습니까?'}
+                    </Typography>
+                    <Stack direction="row" spacing={2} justifyContent='center' alignItems='center'>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleSubmitSoldOut}
+                            className="modalConfirmButton">
+                            확인
+                        </Button>
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            onClick={handleSoldOutModalClose}
+                            className="modalConfirmButton">
+                            취소
+                        </Button>
+                    </Stack>
                 </Box>
             </Modal>
         </Box>
