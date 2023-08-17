0e01a9f8a03b18066152cb019cca4d000cfe9d58
Forky <trulyforky@gmail.com>
2023-08-17 23:15:54 +0900

    feat(ui-owner): 사장님 주문 fcm push 알림 구독 버튼 (#128)
    * feat(ui-owner): 알림 권한 요청 button
    
    * feat(ui-owner): 알림 권한 요청 후 기기 토큰 획득
    
    * feat(ui-owner): fcm push 수신을 위한 service worker 추가
    
    * feat(ui-owner): fcm 기기 토큰으로 서버에 주문 알림 구독 요청

--- frontend/src/function/BrowserNotification.jsx
+++ frontend/src/function/BrowserNotification.jsx
@@ -3,18 +3,7 @@ export const sendNotification = (message) => {
         return;
     }
 
-    if (Notification.permission !== "granted") {
-        Notification
-            .requestPermission()
-            .then((permission) => {
-                if (permission === "granted") {
-                    new Notification(message);
-                } else {
-                    alert("모든 알림이 차단되었습니다.");
-                    return;
-                }
-            });
-    } else {
+    if (Notification.permission === "granted") {
         new Notification(message);
     }
 };
