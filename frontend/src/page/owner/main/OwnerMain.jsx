4a38bf1f7a9d9d0cd7310f232041dc91320de198
YJGwon <trulyforky@gmail.com>
2023-08-17 22:59:43 +0900

    feat(ui-owner): fcm push 수신을 위한 service worker 추가

--- frontend/src/page/owner/main/OwnerMain.jsx
+++ frontend/src/page/owner/main/OwnerMain.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
 import {Container, Button} from '@mui/material';
 import {initializeApp} from "firebase/app";
-import {getMessaging, getToken} from "firebase/messaging";
+import {getMessaging, getToken, onMessage} from "firebase/messaging";
 
 import './OwnerMain.css';
 
 export const OwnerMain = () => {
     const firebaseConfig = {
-        apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-        authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
-        projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
-        storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
-        messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
-        appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`
+        apiKey: 'AIzaSyCnPSnVjn24A4MqVqDYCTX6tKHMUdcyErM',
+        authDomain: 'project-connectruck.firebaseapp.com',
+        projectId: 'project-connectruck',
+        storageBucket: 'project-connectruck.appspot.com',
+        messagingSenderId: '648376554902',
+        appId: '1:648376554902:web:2b661800d226982d7d79b7'
     };
-    
-    const app = initializeApp(firebaseConfig);
-    const messaging = getMessaging(app);
+    initializeApp(firebaseConfig);
+
+    const messaging = getMessaging();
+    onMessage(messaging, (payload) => {
+        console.log('Message received. ', payload);
+    });
 
     const enableFcm = () => {
-        if (!("Notification" in window)) {
-            alert("주문 알림이 지원되지 않는 브라우저입니다.");
-            return;
-        }
-    
-        Notification
-            .requestPermission()
-            .then((permission) => {
-                if (permission === "granted") {
-                    getToken(messaging, {vapidKey: `${process.env.REACT_APP_FIREBASE_VAPID_KEY}`}).then((currentToken) => {
-                        if (currentToken) {
-                            alert("주문 알림이 활성화됩니다.");
-                            console.log("token", currentToken);
-                        } else {
-                            alert("토큰을 가져올 수 없습니다.");
-                        }
-                    }).catch((err) => {
-                        console.log('An error occurred while retrieving token. ', err);
-                    });
-                } else {
-                    alert("모든 알림이 차단되었습니다.");
-                }
-            });
+    if (!("Notification" in window)) {
+        alert("주문 알림이 지원되지 않는 브라우저입니다.");
+        return;
+    }
+    Notification
+        .requestPermission()
+        .then((permission) => {
+            if (permission === "granted") {
+                getToken(messaging, {vapidKey: `${process.env.REACT_APP_FIREBASE_VAPID_KEY}`}).then((currentToken) => {
+                    if (currentToken) {
+                        alert("주문 알림이 활성화됩니다.");
+                        console.log("token", currentToken);
+                    } else {
+                        alert("토큰을 가져올 수 없습니다.");
+                    }
+                }).catch((err) => {
+                    console.log('An error occurred while retrieving token. ', err);
+                });
+            } else {
+                alert("모든 알림이 차단되었습니다.");
+            }
+        });
     }
 
     return (
