9de7e0f294d693d4f1f0685bbe9d58af4884f926
YJGwon <trulyforky@gmail.com>
2023-08-17 23:11:41 +0900

    feat(ui-owner): fcm 기기 토큰으로 서버에 주문 알림 구독 요청

--- frontend/src/page/owner/main/OwnerMain.jsx
+++ frontend/src/page/owner/main/OwnerMain.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Container, Button} from '@mui/material';
 import {initializeApp} from "firebase/app";
 import {getMessaging, getToken, onMessage} from "firebase/messaging";
 
+import {UserContext} from '../../../context/UserContext';
+import {fetchApi} from '../../../function/CustomFetch';
+
 import './OwnerMain.css';
 
 export const OwnerMain = () => {
+    const {accessToken} = useContext(UserContext);
+
     const firebaseConfig = {
         apiKey: 'AIzaSyCnPSnVjn24A4MqVqDYCTX6tKHMUdcyErM',
         authDomain: 'project-connectruck.firebaseapp.com',
@@ -22,29 +27,47 @@ export const OwnerMain = () => {
     });
 
     const enableFcm = () => {
-    if (!("Notification" in window)) {
-        alert("주문 알림이 지원되지 않는 브라우저입니다.");
-        return;
-    }
-    Notification
-        .requestPermission()
-        .then((permission) => {
-            if (permission === "granted") {
-                getToken(messaging, {vapidKey: `${process.env.REACT_APP_FIREBASE_VAPID_KEY}`}).then((currentToken) => {
-                    if (currentToken) {
-                        alert("주문 알림이 활성화됩니다.");
-                        console.log("token", currentToken);
-                    } else {
+        if (!("Notification" in window)) {
+            alert("주문 알림이 지원되지 않는 브라우저입니다.");
+            return;
+        }
+
+        Notification
+            .requestPermission()
+            .then((permission) => {
+                if (permission === "granted") {
+                    getToken(messaging, {vapidKey: `${process.env.REACT_APP_FIREBASE_VAPID_KEY}`}).then((currentToken) => {
+                        if (currentToken) {
+                            subscribeOrders(currentToken);
+                        } else {
+                            throw new Error("토큰을 가져올 수 없습니다.");
+                        }
+                    }).catch((err) => {
                         alert("토큰을 가져올 수 없습니다.");
-                    }
-                }).catch((err) => {
-                    console.log('An error occurred while retrieving token. ', err);
-                });
-            } else {
-                alert("모든 알림이 차단되었습니다.");
-            }
-        });
+                        console.log('An error occurred while retrieving token. ', err);
+                    });
+                } else {
+                    alert("모든 알림이 차단되었습니다.");
+                }
+            });
     }
+
+    const subscribeOrders = async (token) => {
+        const url = `${process.env.REACT_APP_API_URL}/api/notification/orders/my/subscription`;
+        const data = {token};
+        const requestInfo = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+        const onSuccess = () => {
+            alert("주문 알림이 활성화됩니다.");
+        };
+        fetchApi({url, requestInfo}, onSuccess);
+    };
 
     return (
         <Container>
