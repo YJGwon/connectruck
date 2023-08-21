ec0403d542517e9e2f17465596dce14dc0daa00c
YJGwon <trulyforky@gmail.com>
2023-08-22 01:25:06 +0900

    chore(ui-owner): sse 제거

--- frontend/src/function/BrowserNotification.jsx
+++
@@ -1,9 +0,0 @@
-export const sendNotification = (message) => {
-    if (!("Notification" in window)) {
-        return;
-    }
-
-    if (Notification.permission === "granted") {
-        new Notification(message);
-    }
-};
