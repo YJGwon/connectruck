c2668b51680705c614961fe0f5139cb618fd9ad7
YJGwon <trulyforky@gmail.com>
2023-08-22 14:19:35 +0900

    feat(notification): fcm 구독 해제 service 구현

---
+++ backend/foodtruck/src/main/java/com/connectruck/foodtruck/notification/dto/PushUnsubscribeRequest.java
@@ -0,0 +1,13 @@
+package com.connectruck.foodtruck.notification.dto;
+
+import static com.connectruck.foodtruck.common.validation.ValidationMessage.MISSING_REQUIRED_INPUT;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+public record PushUnsubscribeRequest(
+        @Schema(description = "fcm 기기 등록 토큰")
+        @NotNull(message = MISSING_REQUIRED_INPUT)
+        String token
+) {
+}
